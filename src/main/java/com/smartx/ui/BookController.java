3d3ad796851d9275204590c659c23cae9cb3a208
chenjpu <chenjpu@gmail.com>
2021-07-06 10:05:44 +0800

    update config

--- src/main/java/com/smartx/ui/BookController.java
+++ src/main/java/com/smartx/ui/BookController.java
@@ -1,21 +1,12 @@
 package com.smartx.ui;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
 import com.smartx.dao.BookRepository;
 import com.smartx.entity.Book;
 import com.smartx.service.AuthorService;
 
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Post;
-import io.micronaut.http.annotation.Put;
-import io.reactivex.Single;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Controller("/books")
 public class BookController {
@@ -27,38 +18,15 @@ public class BookController {
 		this.authorService = authorService;
 	}
 
-	// tag::create[]
-	@Post("/")
-	Single<Book> create(@Valid Book book) {
-		return Single.fromPublisher(bookRepository.save(book));
-	}
-	// end::create[]
-
 	// tag::read[]
-	@Get("/")
-	Flux<Book> all() {
+	@Get("/tx")
+	Flux<Book> Tx() {
 		return authorService.findAll(); // <1>
-		//return bookRepository.findAll(); // <1>
 	}
-
-	@Get("/{id}")
-	Mono<Book> show(Long id) {
-		return bookRepository.findById(id); // <2>
+	
+	@Get("/ntx")
+	Flux<Book> notTx() {
+		return bookRepository.findAll(); // <1>
 	}
-	// end::read[]
-
-	// tag::update[]
-	@Put("/{id}")
-	Single<Book> update(@NotNull Long id, @Valid Book book) {
-		return Single.fromPublisher(bookRepository.update(book));
-	}
-	// end::update[]
-
-	// tag::delete[]
-	@Delete("/{id}")
-	Single<HttpResponse<?>> delete(@NotNull Long id) {
-		return Single.fromPublisher(bookRepository.deleteById(id))
-				.map(deleted -> deleted > 0 ? HttpResponse.noContent() : HttpResponse.notFound());
-	}
-	// end::delete[]
+	
 }
